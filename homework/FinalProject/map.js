9a9b92a856222dd2b65cb2a3a18108110ddbe7ce
mquinci <milena.quinci@gmail.com>
2021-11-23 20:34:51 -0500

    Updates to map code

--- homework/FinalProject/map.js
+++ homework/FinalProject/map.js
@@ -1,3 +1,71 @@
+var countryList = {
+  "US": "United States"
+};
+
+function drawMap(world, data, variable) {
+    
+  console.log(world);
+  var geoJSON = topojson.feature(world, world.objects.countries);
+  console.log(geoJSON);
+
+  geoJSON.features = geoJSON.features.filter(function(d) {
+    return d.id !== "AQ";
+  });
+
+  var proj = d3.geoMercator()
+    .fitSize([width, height], geoJSON);
+
+  var path = d3.geoPath()
+    .projection(proj);
+
+  var countries = map.selectAll("path")
+    .data(geoJSON.features);
+
+  var enterCountries = countries.enter().append("path")
+    .attr("d", path)
+    .attr("vector-effect", "non-scaling-stroke")
+    .attr("stroke", "cornflowerblue")
+    .attr("stroke-width", "0.5px");
+
+  countries.merge(enterCountries)
+    .attr("fill", function(d) {
+      var countryName = countryList[d.id];
+      if (countryName) {
+        var dataPoint = data.filter(function(a) {
+          return a["Country name"] === countryName
+        });
+
+        if (dataPoint.length) {
+          dataPoint = dataPoint[0];
+          console.log("Found one!", dataPoint);
+          return "green";
+        }
+        else {
+          console.log("Missing Data", countryName);
+          return "red";
+        }
+
+      }
+      else {
+        return "gray";
+      }
+      
+    });
+  console.log(data);
+
+  var zoom = d3.zoom()
+    .translateExtent([[0, 0], [width, height]])
+    .scaleExtent([1, 16])
+    .on("zoom", zoomed);
+
+  function zoomed() {
+    map.attr("transform", d3.event.transform);
+  }
+
+  svg.call(zoom);
+
+}
+
 d3.select("#map")
   .on("mousemove", function() {
 
